fb505b46aa1808e4b114d0c14f8ecf92366c416a
TJChiang <tjchiang8419@gmail.com>
2024-06-06 23:05:18 +0800

    add bcrypt handler

---
+++ internal/bcrypt.go
@@ -0,0 +1,18 @@
+package internal
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func GetHashingCost(hashedPassword []byte) int {
+	cost, _ := bcrypt.Cost(hashedPassword)
+	return cost
+}

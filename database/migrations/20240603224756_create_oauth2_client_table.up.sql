6f09f979d4a16b108178a43e5ddadb74c2bf6650
TJChiang <tjchiang8419@gmail.com>
2024-06-06 23:30:23 +0800

    change datetime

--- database/migrations/20240603224756_create_oauth2_client_table.up.sql
+++ database/migrations/20240603224756_create_oauth2_client_table.up.sql
@@ -11,7 +11,7 @@ create table if not exists oauth2_client
     post_logout_redirect_uris json not null default ('[]'),
     backchannel_logout_uri text not null default (''),
     redirect_uris json not null default ('[]'),
-    created_at timestamp default current_timestamp() not null,
-    updated_at timestamp default current_timestamp() on update current_timestamp() not null,
+    created_at int unsigned not null default (unix_timestamp()),
+    updated_at int unsigned not null default (unix_timestamp()),
     primary key(id)
 ) ENGINE=InnoDB default CHARSET=utf8mb4 collate=utf8mb4_unicode_ci;
